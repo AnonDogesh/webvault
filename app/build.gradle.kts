afa3f02b38119c50267536770d22ae9841bdd25b
AnonDogesh <DivineLeaf@proton.me>
2026-03-19 22:09:46 +0530

    Add Material Components dependency for XML Material3 theme parent

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material)
+    implementation(libs.google.material)
 
     implementation(libs.androidx.work.runtime.ktx)
     implementation(libs.androidx.security.crypto)
